526f5aec1ecec3508d500d1c52187f2746741b85
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-21 14:33:30 -0300

    feat: adding mongoose

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(error => console.log(error.message));
+
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Salmon Creek",
+//     image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350"
+// });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -9,26 +27,30 @@ app.get("/", (req, res) => {
     res.render("landing");
 });
 
-const campgrounds = [
-    { name: "Salmon Creek", image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350" },
-    { name: "Granite Hill", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350" }
-];
-
 app.get("/campgrounds", (req, res) => {
-    
-    res.render("campgrounds", { campgrounds });
+    Campground.find({}, (err, campgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", { campgrounds });
+        }
+    });
 });
 
 app.post("/campgrounds", (req, res) => {
     const name = req.body.name;
     const image = req.body.image;
 
-    campgrounds.push({
+    Campground.create({
         name,
         image
+    }, (err, campground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
     });
-    
-    res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", (req, res) => {
