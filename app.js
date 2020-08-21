a087816d8f99f05eb5f748dc9c27aebdca8dfab3
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-21 14:57:23 -0300

    feat: campground show page

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 
 const campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
-//     name: "Salmon Creek",
-//     image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350"
+//     name: "Granite Hill",
+//     image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350",
+//     description: "This is a huge granite hill, no bathrooms, no water"
 // });
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,7 +34,7 @@ app.get("/campgrounds", (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds", { campgrounds });
+            res.render("index", { campgrounds });
         }
     });
 });
@@ -40,10 +42,12 @@ app.get("/campgrounds", (req, res) => {
 app.post("/campgrounds", (req, res) => {
     const name = req.body.name;
     const image = req.body.image;
+    const description = req.body.description;
 
     Campground.create({
         name,
-        image
+        image,
+        description
     }, (err, campground) => {
         if (err) {
             console.log(err);
@@ -55,6 +59,17 @@ app.post("/campgrounds", (req, res) => {
 
 app.get("/campgrounds/new", (req, res) => {
     res.render("new.ejs");
+})
+
+app.get("/campgrounds/:id", (req, res) => {
+    
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground});
+        }
+    });
 })
 
 app.listen(3000);
