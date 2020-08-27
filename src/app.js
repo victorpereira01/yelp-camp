ebea4b62e8d817d6b773b55573e4c995129a3509
Victor Pereira <victor.cordeiro2@gmail.com>
2020-08-26 22:29:34 -0300

    feat: middleware for preventing user adding a comment not logged in

--- src/app.js
+++ src/app.js
@@ -89,7 +89,7 @@ app.get("/campgrounds/:id", (req, res) => {
 // ===================
 // Comment Routes
 // ===================
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if (err) {
             console.log(err);
@@ -99,7 +99,7 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
     });
 })
 
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if (err) {
             res.redirect("/campgrounds");
@@ -159,5 +159,13 @@ app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/campgrounds');
 })
+
+// middleware
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.listen(3000);
